80980b6fd3dc996f92e43b4d46b6b97562e2f113
金泰熙 <115124204+jintaixihaw@users.noreply.github.com>
2022-10-06 09:35:59 +0800

    Update cache_helper.js

--- miniprogram/helper/cache_helper.js
+++ miniprogram/helper/cache_helper.js
@@ -1,5 +1,5 @@
 /**
- * Notes: 微信缓存二次封装，有设置时效性的封装
+ * Notes:  微信缓存二次封装，有设置时效性的封装
  * Ver : CCMiniCloud Framework 3.0.1 ALL RIGHTS RESERVED BY cclinux0730 (wechat)
  * Date: 2020-11-14 07:48:00 
  */
